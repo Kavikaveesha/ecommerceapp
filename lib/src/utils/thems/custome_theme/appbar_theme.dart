e65a414a7b5f928990c42184f43415d37e0372cb
Kavikaveesha <140581819+Kavikaveesha@users.noreply.github.com>
2023-12-30 00:38:22 +0530

    create themes

---
+++ lib/src/utils/thems/custome_theme/appbar_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyAppbarTheme {
+  MyAppbarTheme._();
+
+  static const lightAppBarTheme = AppBarTheme(
+    elevation: 0,
+    centerTitle: f
+  );
+}
---
+++ lib/src/utils/thems/theme.dart
@@ -0,0 +1,30 @@
+import 'package:ecommerceapp/src/utils/thems/custome_theme/elevated_button_theme.dart';
+import 'package:ecommerceapp/src/utils/thems/custome_theme/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class MyAppTheme {
+  MyAppTheme._();
+
+// cretate theme data to light mood
+  static ThemeData lightThemeData = ThemeData(
+      useMaterial3: true,
+      fontFamily: 'poppins',
+      brightness: Brightness.light,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: MyAppTextTheme.lightTextTheme,
+      elevatedButtonTheme: MyAppElevatedButtonTheme.lightElevatedButtonTheme);
+// cretate theme data to light mood
+
+// cretate theme data to dark mood
+  static ThemeData darkThemeData = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: MyAppTextTheme.darkTextTheme,
+    elevatedButtonTheme: MyAppElevatedButtonTheme.darkElevatedButtonTheme,
+  );
+// cretate theme data to dark mood
+}
